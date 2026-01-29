2cffd1b81f4709eec2573d7b56a881e1e1993842
Mayuresh Kamble <mackam30@gmail.com>
2026-01-29 21:19:50 +0530

    solved one question

--- one_shot.py
+++ one_shot.py
@@ -4,17 +4,32 @@
 
 # print(name, age, isGenius)
 
-def person():
-    name = "Tony Stark"
-    age = 51
-    isGenius = True
-    # return(name , age , isGenius)
-    print(name, age, isGenius)
+# def person():
+#     name = "Tony Stark"
+#     age = 51
+#     isGenius = True
+#     # return(name , age , isGenius)
+#     print(name, age, isGenius)
 
-person()
+# person()
 
-def secretName():
-    secret = input("What is ur secret name: ")
-    print(secret)
+# def secretName():
+#     secret = input("What is ur secret name: ")
+#     print(secret)
 
-secretName()
+# secretName()
+
+# def oldAge():
+#     old_age = int(input("Enter Your age: "))
+#     new_age = old_age + 2
+#     print(new_age)
+
+# oldAge()
+
+def addition():
+    first = int(input("Enter your 1st no. : "))
+    second = int(input("Enter Your 2nd no. : "))
+    sum = first + second
+    print("The sum is ", sum)
+
+addition()
