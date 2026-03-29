80829d65d69b1867ce186d01546c886b2fce6b80
Maaaccc05 <mackam30@gmail.com>
2026-03-29 23:34:54 +0530

    mini project done

--- OpenCV/Mini-Project/project-1.py
+++ OpenCV/Mini-Project/project-1.py
@@ -2,12 +2,24 @@ import cv2
 
 image = cv2.imread("OpenCV/minato-namikaze-3840x2160-24353.png")
 
-if image is not None:
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    input("Do you want to save or Show")
-    if 
+if image is None:
+    print("Error: Image not found")
+    exit()
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+choice = input("Type 'show' to display and 'save' to Save the image ").lower()
+
+if choice == "show":
+    cv2.imshow("GrayScaled Image", gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-else:
-    print("Could not convert")
 
+elif choice == "save":
+    filename = input("Enter File name to save with (.png or .jpg)")
+
+    cv2.imwrite(filename, gray)
+    print("Image is saved as {filename}")
+
+else:
+    print("Invalid Choice type again")
