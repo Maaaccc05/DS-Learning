0180e7a5a042d3f12067c1e6b2239d895ce4fdc9
Maaaccc05 <mackam30@gmail.com>
2026-04-02 20:22:48 +0530

    Drawed rectangle on image

--- OpenCV/Phase-3-Image Transformation and Manipulation/drawing_line.py
+++ OpenCV/Phase-3-Image Transformation and Manipulation/drawing_line.py
@@ -1,19 +1,37 @@
 import cv2
-image = cv2.imread("OpenCV/minato-namikaze-3840x2160-24353.png")
+image = cv2.imread("OpenCV/dmxffni837f1xrj8pki9xgrl.jpg")
+
+# if image is None:
+#     print("Not found")
+
+# else:
+#     print("Image Loaded")
+#     pt1 = (50,50)
+#     pt2 = (400, 55)
+#     color = (0, 0, 255)
+#     thickness = 5
+
+#     cv2.line(image, pt1,pt2, color, thickness)
+
+#     cv2.imshow("Line image", image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# Drawing rectangle 
+# syntax: cv2.rectangle(image, pt1, pt2, color, thickness)
 
 if image is None:
     print("Not found")
 
 else:
-    resized = cv2.resize(image, (500, 500))
     print("Image Loaded")
-    pt1 = (50,50)
-    pt2 = (400, 55)
+    pt1 = (250, 30)
+    pt2 = (600, 350)
     color = (0, 0, 255)
-    thickness = 5
+    thickness = 3
 
-    cv2.line(resized, pt1,pt2, color, thickness)
+    cv2.rectangle(image, pt1, pt2, color, thickness)
 
-    cv2.imshow("Line image", resized)
+    cv2.imshow("Rectangle", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
