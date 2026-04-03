49657fa0314bed19dbcac55cb7da3559b063d6a0
Maaaccc05 <mackam30@gmail.com>
2026-04-04 00:10:07 +0530

    drawed line on image by user values

--- OpenCV/Mini-Project/project-2.py
+++ OpenCV/Mini-Project/project-2.py
@@ -7,10 +7,22 @@ image = cv2.imread(file_path)
 choice = input("1.Type 'line' for drawing line on image\n2.Type 'rectangle' to draw rectangle on image\n3.Type 'circle' to draw circle on image\n4.Type 'text' to add tect on image ").lower()
 
 if choice == 'line':
-    pt1 = input("Enter the point from where you want line")
-    pt2 = input("Enter the point till where you want line")
-    color = input("Choose color in format of (0, 0, 0)")
-    thickness = input("Choose thickness")
+    x1 = int(input("Enter x1 value"))
+    y1 = int(input("Enter y1 value"))
+    pt1 = (x1, y1)
+    x2 = int(input("Enter x2 value"))
+    y2 = int(input("Enter y2 value")) 
+    pt2 = (x2, y2)
+    b = int(input("Enter B value of the color"))
+    g = int(input("Enter G value of the color"))
+    r = int(input("Enter R value of the color"))
+    color = (b, g, r)
+    thickness = int(input("Choose thickness"))
     cv2.line(image, pt1,pt2, color, thickness)
+
+    cv2.imshow("Line On Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 else:
     print("Wrong text")
--- OpenCV/Phase-3-Image Transformation and Manipulation/drawing.py
+++ OpenCV/Phase-3-Image Transformation and Manipulation/drawing.py
@@ -20,52 +20,52 @@ else:
 # Drawing rectangle 
 # syntax: cv2.rectangle(image, pt1, pt2, color, thickness)
 
-# if image is None:
-#     print("Not found")
+if image is None:
+    print("Not found")
 
-# else:
-#     print("Image Loaded")
-#     pt1 = (250, 30)
-#     pt2 = (600, 350)
-#     color = (0, 0, 255)
-#     thickness = 3
+else:
+    print("Image Loaded")
+    pt1 = (250, 30)
+    pt2 = (600, 350)
+    color = (0, 0, 255)
+    thickness = 3
 
-#     cv2.rectangle(image, pt1, pt2, color, thickness)
+    cv2.rectangle(image, pt1, pt2, color, thickness)
 
-#     cv2.imshow("Rectangle", image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    cv2.imshow("Rectangle", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# # Drawing Circle
-# # Syntax: cv2.circle(imgae, center, radius, color, thickness)
+# Drawing Circle
+# Syntax: cv2.circle(imgae, center, radius, color, thickness)
 
-# if image is None:
-#     print("Not found")
+if image is None:
+    print("Not found")
 
-# else:
-#     print("Image Loaded")
-#     center = (400, 200)
-#     radius = 200
-#     color = (0, 0, 255)
-#     thickness = 3  # thickness can be -1 if you want to fill your cicle
+else:
+    print("Image Loaded")
+    center = (400, 200)
+    radius = 200
+    color = (0, 0, 255)
+    thickness = 3  # thickness can be -1 if you want to fill your cicle
 
-#     cv2.circle(image, center, radius, color, thickness)
+    cv2.circle(image, center, radius, color, thickness)
 
-#     cv2.imshow("Circle", image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    cv2.imshow("Circle", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# # Adding Text
-# # syntanx: cv2.putText(image, text, org, font, fontScale, color, thickness)
+# Adding Text
+# syntanx: cv2.putText(image, text, org, font, fontScale, color, thickness)
 
-# if image is None:
-#     print("Not found")
+if image is None:
+    print("Not found")
 
-# else:
-#     print("Image Loaded")
+else:
+    print("Image Loaded")
 
-#     cv2.putText(image, "Hello OpenCV", (250, 400), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0, 200, 255))
+    cv2.putText(image, "Hello OpenCV", (250, 400), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0, 200, 255))
 
-#     cv2.imshow("Rectangle", image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    cv2.imshow("Rectangle", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
