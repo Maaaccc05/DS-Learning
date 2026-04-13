3a5ef30b39cd18218219a598bdfeb2c4da089e47
Maaaccc05 <mackam30@gmail.com>
2026-04-13 21:30:08 +0530

    Made Image blurred

--- OpenCV/Phase-5-Image Filetering and  Blurring/image_blurring.py
+++ OpenCV/Phase-5-Image Filetering and  Blurring/image_blurring.py
@@ -1,2 +1,13 @@
 # Syntax: cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma)
 
+import cv2
+
+image = cv2.imread("OpenCV/landscape-lake-mountain-clear-nature-wallpaper-preview.jpg")
+
+blur = cv2.GaussianBlur(image, (7, 7), 0)
+
+cv2.imshow("Original",image)
+cv2.imshow("Blurr", blur)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
