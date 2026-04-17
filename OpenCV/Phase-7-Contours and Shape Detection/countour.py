bc5ef0f1fd5efb95dbdc4df6d4bcdd5b1ac1bbb0
Maaaccc05 <mackam30@gmail.com>
2026-04-17 21:04:35 +0530

    Moving forward  Contours and Shape detection Phase-7

---
+++ OpenCV/Phase-7-Contours and Shape Detection/countour.py
@@ -0,0 +1,5 @@
+"""
+    Syntax of Countours: contours, hierarchy = cv2.findContours(img, mode, method)
+    Syntax for Draw Contoirs: cv2.drawContours(img, contours, contour_index, color, thickness)
+
+"""
